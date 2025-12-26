fb99a9f196f482ed7eb3c1ff0458298ba946e798
mami <mamypoco@gmail.com>
2025-12-26 13:35:50 -0800

    add styling for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,11 @@ function App() {
         <Board />
       </aside>
       <main>
-        <h1>It always seems impossible</h1>
-        <h1>Until it is done</h1>
+        <div className='quote-writing'>
+          <h1>It always seems impossible</h1>
+          <h1>Until it is done</h1>
+        </div>
+        {/* <CardList /> */}
       </main>
     </div>
     </>
