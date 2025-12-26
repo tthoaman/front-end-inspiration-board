a34ac59d11599fbeaeee3528a0a25d6b9d244ce3
mami <mamypoco@gmail.com>
2025-12-26 14:47:18 -0800

    centered the selected board title

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,21 @@ import Board from './components/boards/Board';
 function App() {
 
   return (
-    <>
     <div className='app'>
       <aside className="sidebar">
         <Board />
       </aside>
       <main>
-        <div className='board-writing'>
+        <div className="board-writing">
           <h1 className="left-writing">It always seems impossible</h1>
           <h1 className="right-writing">Until it is done</h1>
+        </div>
+        <div className="selected-board-title">
+          <h1>Christmas Family Wishlist</h1>
         </div>
         {/* <CardList /> */}
       </main>
     </div>
-    </>
   )
 }
 
