1175fab14ba53edb4236e4c8a9d1641cebd15aa4
liyan <liyangeneral@gmail.com>
2025-12-26 23:57:53 -0800

    add NewBoardForm component with controlled inputs for title and owner

---
+++ src/components/boards/NewBoardForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+
+const kDefaultFormState = {
+  title: '',
+  owner: ''
+};
+
+const NewBoardForm = ({ onHandleSubmit }) => {
+  const [formData, setFormData] = useState(kDefaultFormState);
+
+  const handleChange = (event) => {
+    const inputValue = event.target.value;
+    const inputName = event.target.name;
+    setFormData(formData => {
+      return {
+        ...formData,
+        [inputName]: inputValue
+      };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newBoard = {
+      ...formData
+    };
+    onHandleSubmit(newBoard);
+    setFormData(kDefaultFormState);
+  };
+
+  const makeControlledInput = (inputName) => {
+    return (
+      <div>
+        <label htmlFor={inputName}>
+          {inputName}
+        </label>
+        <input
+          type='text'
+          name={inputName}
+          id={inputName}
+          value={formData[inputName]}
+          onChange={handleChange}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <h1>Create a board</h1>
+      <form onSubmit={handleSubmit}>
+        {makeControlledInput('title')}
+        {makeControlledInput('owner')}
+
+        <div>
+          <input type="submit" value="Create board" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default NewBoardForm;
